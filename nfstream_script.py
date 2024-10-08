b24707b603a882c95b2f36d2f8bfb316db19fc1a
Miel Verkerken <miel.verkerken@ugent.be>
2024-10-08 18:57:20 +0200

    add output folder to nfstream script

--- nfstream_script.py
+++ nfstream_script.py
@@ -3,7 +3,7 @@ import argparse
 import os
 
 # Function to process offline mode (PCAP file)
-def process_offline(pcap_file):
+def process_offline(pcap_file, output_folder):
     print(f"NFStream processing offline PCAP file: {pcap_file}")
     
     # Output CSV file based on the PCAP file name
@@ -17,10 +17,10 @@ def process_offline(pcap_file):
                              statistical_analysis=True)
 
     # Write the flow results to CSV
-    my_streamer.to_csv(output_csv)
+    my_streamer.to_csv(os.path.join(output_folder, output_csv))
 
 # Function to process real-time mode (Network Interface)
-def process_realtime(interface):
+def process_realtime(interface, output_folder):
     print(f"NFStream processing in real-time on interface: {interface}")
     
     # NFStreamer for real-time traffic capture
@@ -35,7 +35,7 @@ def process_realtime(interface):
     output_csv = f"nfstream_{os.path.basename(interface)}.csv"
     
     # Write the flow results to CSV
-    my_streamer.to_csv(output_csv)
+    my_streamer.to_csv(os.path.join(output_folder, output_csv))
 
 # Main function
 if __name__ == "__main__":
@@ -45,13 +45,14 @@ if __name__ == "__main__":
     mode_group = parser.add_mutually_exclusive_group(required=True)
     mode_group.add_argument("--offline", type=str, help="Path to the PCAP file for offline processing.")
     mode_group.add_argument("--realtime", type=str, help="Network interface for real-time traffic processing.")
+    mode_group.add_argument("--output", type=str, help="The output folder for the CSV file with the flow results.")
     
     args = parser.parse_args()
 
     # Offline mode: process the PCAP file
     if args.offline:
-        process_offline(args.offline)
+        process_offline(args.offline, args.output)
 
     # Real-time mode: process the network interface
     elif args.realtime:
-        process_realtime(args.realtime)
+        process_realtime(args.realtime, args.output)
