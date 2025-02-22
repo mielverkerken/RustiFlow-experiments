0e7ce50b8a04d445642537a16e90535277ef5ba8
Miel Verkerken <miel.verkerken@ugent.be>
2025-02-22 22:48:39 +0100

    update

--- replay_performance_test.py
+++ replay_performance_test.py
@@ -13,7 +13,7 @@ import json
 exporters = {
     "rustiflow": {
         "name": "rustiflow",
-        "shell": False,
+        "shell": True,
         "cmd": "sudo ./rustiflow -f rustiflow --header --idle-timeout 120 --active-timeout 3600 --performance-mode --output csv --export-path {output_folder}/rustiflow_realtime.csv realtime {interface} > {output_folder}/rustiflow_realtime_{throughput}.log 2>&1",
         "cwd": "/users/mverkerk/RustiFlow/target/release/",
     },
@@ -55,7 +55,7 @@ exporters = {
     },
     "nprobe": {
         "name": "nprobe",
-        "shell": False,
+        "shell": True,
         "cmd": "sudo nprobe -i {interface} -n none -t 3600 -d 120 --csv-separator , -P {output_folder} -V 9  -T '%IPV4_SRC_ADDR %IPV4_DST_ADDR %L4_SRC_PORT %L4_DST_PORT %PROTOCOL %L7_PROTO %IN_BYTES %OUT_BYTES %IN_PKTS %OUT_PKTS %FLOW_DURATION_MILLISECONDS %TCP_FLAGS %CLIENT_TCP_FLAGS %SERVER_TCP_FLAGS %DURATION_IN %DURATION_OUT %MIN_TTL %MAX_TTL %LONGEST_FLOW_PKT %SHORTEST_FLOW_PKT %MIN_IP_PKT_LEN %MAX_IP_PKT_LEN %SRC_TO_DST_SECOND_BYTES %DST_TO_SRC_SECOND_BYTES %RETRANSMITTED_IN_BYTES %RETRANSMITTED_IN_PKTS %RETRANSMITTED_OUT_BYTES %RETRANSMITTED_OUT_PKTS %SRC_TO_DST_AVG_THROUGHPUT %DST_TO_SRC_AVG_THROUGHPUT %NUM_PKTS_UP_TO_128_BYTES %NUM_PKTS_128_TO_256_BYTES %NUM_PKTS_256_TO_512_BYTES %NUM_PKTS_512_TO_1024_BYTES %NUM_PKTS_1024_TO_1514_BYTES %TCP_WIN_MAX_IN %TCP_WIN_MAX_OUT %ICMP_TYPE %ICMP_IPV4_TYPE %DNS_QUERY_ID %DNS_QUERY_TYPE %DNS_TTL_ANSWER %FTP_COMMAND_RET_CODE' > {output_folder}/nprobe_realtime_{throughput}.log 2>&1",
         "cwd": None,  # Runs from any directory
     },
